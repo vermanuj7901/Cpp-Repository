f41827a79d4106c6da64e5744e1bdb372f932600
vermanuj7901 <anujravinderverma403@gmail.com>
2022-10-03 17:18:45 +0530

    Update 3.1

---
+++ Recursion-Factorial/app.cpp
@@ -0,0 +1,22 @@
+//written by anuj
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    int ans = n;
+    if(n==0){
+        return 1;
+    }
+    else{
+        ans = ans*fact(n-1);
+        return ans;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<n<<"! = "<<fact(n)<<endl;
+    cout<<"\nThank You for using the application (Written by ANUJ VERMA)"<<endl;
+}
