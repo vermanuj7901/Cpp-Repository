3ef34880925273f2f34dc4287aad97c27e6a2471
vermanuj7901 <anujravinderverma403@gmail.com>
2022-10-03 18:20:21 +0530

    Update 3.1

---
+++ Recusion-Increasing or decreasing/app.cpp
@@ -0,0 +1,32 @@
+//written by anuj
+#include<bits/stdc++.h>
+using namespace std;
+
+void inc(int n){
+    if(n==1){
+        return;
+    }
+    inc(n-1);
+    cout<<n<<",";
+}
+
+void dec(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<",";
+    dec(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<"Increasing order: ";
+    inc(n);
+    cout<<endl;
+    cout<<"Decreasing order: ";
+    dec(n);
+    cout<<endl;
+    cout<<"\nThank You for using the application (Written by ANUJ VERMA) "<<endl;
+}
